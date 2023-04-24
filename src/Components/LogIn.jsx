63bd3ba1645321c4378a3e0d302ebb5661fe5464
Mahbubaly <mahbubalikhanofficial@gmail.com>
2023-04-25 03:35:19 +0600

    phone added

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -21,66 +21,80 @@ const LogIn = () => {
 
     return (
         <>
-            <div className=" flex justify-center items-center  bg-white">
-                <div className="hero-content flex-col lg:w-[40%] ">
-                    <div className="text-center scroll-container">
-                        <h1 className="text-5xl scroll-content font-bold text-primary my-7">Hello Login now!</h1>
 
-
-
-
+            <div className='flex justify-center mt-7'>
+                <div className="mockup-phone  border-primary">
+                    <div className="camera"></div>
+                    <div className="display">
+                        <div className="artboard artboard-demo phone-1 text-primary font-xl">Loading... </div>
+                        <h1>scroll/</h1>
                     </div>
-                    <div className="card  w-full  bg-base-100">
-                        <form onSubmit={handlerLog} className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text text-[16px] font-semibold">Email</span>
-                                </label>
-                                <input type="text" name='email' placeholder="Email" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text text-[16px] font-semibold">Password</span>
-                                </label>
-                                <input type="password" required name='password' placeholder="password" className="input input-bordered" />
-                                <label className="label">
-
-                                    <Link to="/forgotPass" className='text-primary mt-3 hover:underline'>Forgot password? </Link>
-                                </label>
-                            </div>
-                            <div className="form-control ">
+                </div>
+            </div>
 
 
-                                <button onClick={handlerLog} className="btn btn-primary text-[18px] text-white">
-                                    Log in</button>
+            <div className=''>
+                <div className=" flex justify-center items-center  bg-white">
+                    <div className="hero-content flex-col lg:w-[40%] ">
+                        <div className="text-center scroll-container">
+                            <h1 className="text-5xl scroll-content font-bold text-primary my-7">Hello Login now!</h1>
 
 
-                            </div>
-
-                        </form>
-                        <div className='flex p-3 gap-2 2xl:justify-center'>
 
 
-                            <Link className='flex p-2 justify-center items-center  rounded-lg bg-slate-50 text-center ' to='/google'>
-                                <img className='w-[40px] h-[30px]' src={googleLogo} alt="" />
-                                <h1>Log in with Google</h1>
-
-
-                            </Link>
-                            <Link className='flex  p-2 justify-center items-center rounded-lg bg-slate-50 text-center ' to='/facebook'>
-                                <img className='w-[40px] h-[30px]' src={facebook} alt="" />
-                                <h1>Log in with Facebook</h1>
-
-
-                            </Link>
                         </div>
+                        <div className="card  w-full  bg-base-100">
+                            <form onSubmit={handlerLog} className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text text-[16px] font-semibold">Email</span>
+                                    </label>
+                                    <input type="text" name='email' placeholder="Email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text text-[16px] font-semibold">Password</span>
+                                    </label>
+                                    <input type="password" required name='password' placeholder="password" className="input input-bordered" />
+                                    <label className="label">
+
+                                        <Link to="/forgotPass" className='text-primary mt-3 hover:underline'>Forgot password? </Link>
+                                    </label>
+                                </div>
+                                <div className="form-control ">
+
+
+                                    <button onClick={handlerLog} className="btn btn-primary text-[18px] text-white">
+                                        Log in</button>
+
+
+                                </div>
+
+                            </form>
+                            <div className='flex p-3 gap-2 2xl:justify-center'>
+
+
+                                <Link className='flex p-2 justify-center items-center  rounded-lg bg-slate-50 text-center ' to='/google'>
+                                    <img className='w-[40px] h-[30px]' src={googleLogo} alt="" />
+                                    <h1>Log in with Google</h1>
+
+
+                                </Link>
+                                <Link className='flex  p-2 justify-center items-center rounded-lg bg-slate-50 text-center ' to='/facebook'>
+                                    <img className='w-[40px] h-[30px]' src={facebook} alt="" />
+                                    <h1>Log in with Facebook</h1>
+
+
+                                </Link>
+                            </div>
+                        </div>
+
+
+                        <div>
+                            <Link to='/signUp'> <h1 className='font-semibold hover:underline'>Don’t have account? <span className='text-primary'>Sign up</span></h1> </Link>
+                        </div>
+
                     </div>
-
-
-                    <div>
-                        <Link to='/signUp'> <h1 className='font-semibold hover:underline'>Don’t have account? <span className='text-primary'>Sign up</span></h1> </Link>
-                    </div>
-
                 </div>
             </div>
         </>
